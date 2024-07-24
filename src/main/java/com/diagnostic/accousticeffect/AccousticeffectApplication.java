26e84cafc2521d0aa6ba10de769d6536b021923b
onditivo <onditivo@yahoo.com>
2024-07-24 16:42:01 +0100

    Initial setup from spring template

---
+++ src/main/java/com/diagnostic/accousticeffect/AccousticeffectApplication.java
@@ -0,0 +1,13 @@
+package com.diagnostic.accousticeffect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccousticeffectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccousticeffectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diagnostic/accousticeffect/AccousticeffectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagnostic.accousticeffect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccousticeffectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
